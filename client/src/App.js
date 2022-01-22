ae63567dfe33c7854abf9e5fc3c759785a1fff5d
Kaleb Meeks <krmeeks15@gmail.com>
2022-01-21 18:38:55 -0600

    Initial cleanup and refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Guides from "./components/Guides";
 import Pagination from "./components/Pagination";
 import "./App.css";
-const axios = require("axios");
+import { API_ENDPOINT } from "./constants";
 
 function App() {
-  const [allItems, addItem] = useState([{}]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [guidesPerPage, setGuidesPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
+  const guidesPerPage = 10;
+  let lastGuideIndex = 0;
+  let firstGuideIndex = 0;
+  let currentGuides = [];
+  let returnedItems = [];
 
   useEffect(() => {
-    const fetchItems = async() => {
-      setLoading(true);
-      const response = await axios.get("http://localhost:5000/");
-      addItem(response.data);
-      setLoading(false);
-    }
-    
-    fetchItems();
+    const fetchItems = async () => {
+      const response = await fetch(API_ENDPOINT);
+      const responseJson = await response.json();
+      return responseJson;
+    };
+
+    fetchItems().then((response) => {
+      returnedItems.push(response);
+    });
   }, []);
 
-  // Get current guides
-  const indexOfLastGuide = currentPage * guidesPerPage;
-  const indexOfFirstGuide = indexOfLastGuide - guidesPerPage;
-  const currentGuides = allItems.slice(indexOfFirstGuide, indexOfLastGuide);
+  if (returnedItems.size > 0) {
+    // Get current guides
+    console.log("IF STATEMENT");
+    console.log(returnedItems);
+    lastGuideIndex = currentPage * guidesPerPage;
+    firstGuideIndex = lastGuideIndex - guidesPerPage;
+    currentGuides = returnedItems.slice(firstGuideIndex, lastGuideIndex);
+  }
 
-  // Paginate function 
+  // Paginate function
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
-  }
+  };
 
   return (
     <div className="container">
       <h1>Published Guides</h1>
-      <Guides allItems={currentGuides} loading={loading}/>
-      <Pagination guidesPerPage={guidesPerPage} totalGuides={allItems.length} paginate={paginate}/>
+      <Guides allItems={currentGuides} />
+      <Pagination
+        guidesPerPage={guidesPerPage}
+        totalGuides={returnedItems.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
